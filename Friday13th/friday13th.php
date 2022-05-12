467a33e5b73af81ebadc9e24e180119a091f3911
Milan Ivic <millan.ivic2343@gmail.com>
2022-05-12 16:35:29 +0200

    promenio ime varijable

--- Friday13th/friday13th.php
+++ Friday13th/friday13th.php
@@ -12,8 +12,8 @@ class Friday13thCalculator {
     function calcMethod() {
         for($year = $this->startYear; $year <= $this->endYear; $year++) {
             for($month = 1; $month <= 12; $month++) {
-                $date1 = date_create("$year-$month-01");
-                        $_13th = date_format($date1, '13/M/Y');
+                $date = date_create("$year-$month-01");
+                        $_13th = date_format($date, '13/M/Y');
                         $l_day = date('l', mktime(0,0,0,$month,13,$year)); 
                            if($l_day == 'Friday') {
                                echo $l_day. " the ". $_13th. " ". "<br>";

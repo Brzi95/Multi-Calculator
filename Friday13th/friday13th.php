9cfdcbe59ac973c19a53ce103b662e5ef3013fef
Milan Ivic <millan.ivic2343@gmail.com>
2022-05-13 11:03:52 +0200

    friday13th: set if statement for the years

--- Friday13th/friday13th.php
+++ Friday13th/friday13th.php
@@ -10,20 +10,23 @@ class Friday13thCalculator {
     }
 
     function calcMethod() {
-        for($year = $this->startYear; $year <= $this->endYear; $year++) {
-            for($month = 1; $month <= 12; $month++) {
-                $date = date_create("$year-$month-01");
-                $_13th = date_format($date, '13/M/Y');
-                $l_day = date('l', mktime(0,0,0,$month,13,$year)); 
-                    if($l_day == 'Friday') {
-                    echo $l_day. " the ". $_13th. " ". "<br>";
-                    }
+        if ($this->startYear > 999 && $this->endYear > 999) {
+            for($year = $this->startYear; $year <= $this->endYear; $year++) {
+                for($month = 1; $month <= 12; $month++) {
+                    $date = date_create("$year-$month-01");
+                    $_13th = date_format($date, '13/M/Y');
+                    $l_day = date('l', mktime(0,0,0,$month,13,$year)); 
+                        if($l_day == 'Friday') {
+                        echo $l_day. " the ". $_13th. " ". "<br>";
+                        }
+                }
+                echo "<br>";
             }
-            echo "<br>";
+        } else {
+            echo "Please enter years from 1000 and above!";
         }
     }
 }
-
 
 
 ?>

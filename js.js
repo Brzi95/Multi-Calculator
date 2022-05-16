837dff845521bf4489b104568af8b6d5d8e1847b
Milan Ivic <millan.ivic2343@gmail.com>
2022-05-16 16:17:29 +0200

    .

---
+++ js.js
@@ -0,0 +1,14 @@
+const firstTab = document.querySelector("#tab1");
+
+function activeTab(e) {
+    e.preventDefault();
+    console.log(e.currentTarget);
+    
+    if (firstTab.classList.contains("active-link")) {
+        firstTab.classList.remove("active-link");
+    } else {
+        firstTab.classList.add("active-link");
+    }
+}
+
+firstTab.addEventListener('click', activeTab);

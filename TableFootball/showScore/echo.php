238e67065728b1ea76ac4c9bb41613697dfc6eb0
Milan Ivic <millan.ivic2343@gmail.com>
2022-05-31 19:41:20 +0200

    added JOIN to sql

--- TableFootball/showScore/echo.php
+++ TableFootball/showScore/echo.php
@@ -2,18 +2,14 @@
 
 if ($_POST && $_POST['action'] == 'showScore') {
     $id = $_POST['id'];
-    $sql = "SELECT * FROM brzi_gorance WHERE brzi_gorance.id='$id'";
+   // $sql = "SELECT brzi_gorance.id, user_first FROM brzi_gorance JOIN players ON brzi_gorance.id = players.id WHERE players.id = 1";
+    $sql = "SELECT * FROM brzi_gorance JOIN players ON brzi_gorance.id = players.id WHERE players.id = $id";
     $result = mysqli_query($conn, $sql);
     $resultCheck = mysqli_num_rows($result);
-
-    if ($resultCheck > 0) {
-        while ($row = mysqli_fetch_assoc($result)) {
-            echo "Player $id : wins(" . $row['win'] . ") losses(" . $row['loss'] . ")" . '<br>';
-        }
-           // echo '<br>'. $resultCheck;  row numbers
-    }  
+    while ($row = mysqli_fetch_assoc($result)) {
+        echo "ID $id - " . $row['user_first'] . " : wins(" . $row['win'] . ") losses(" . $row['loss'] . ")" . '<br>';
+    }
 } else {
     echo "Enter player ID to see its score!";
     }
-
 ?>

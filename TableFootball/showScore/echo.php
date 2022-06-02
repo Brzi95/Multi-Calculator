2441bf0700715757fd02dc7d60438045f41a3f35
Milan Ivic <millan.ivic2343@gmail.com>
2022-06-02 14:19:29 +0200

    showScore: radio input works

--- TableFootball/showScore/echo.php
+++ TableFootball/showScore/echo.php
@@ -14,17 +14,37 @@ while ($row_Sum = mysqli_fetch_assoc($sum_Result)) {
 if ($_POST && $_POST['action'] == 'showScore') {
     $inputDate1 = date($_POST['date1']);
     $inputDate2 = date($_POST['date2']);
-    $sql = "SELECT * FROM `brzi-gorance` WHERE `playedOn` BETWEEN '$inputDate1' AND '$inputDate2'";
-    $result = mysqli_query($conn, $sql);
-    $resultCheck = mysqli_num_rows($result);
+    $inputRadio = $_POST['score'];
 
-        echo "between $inputDate1 and $inputDate2 <br><br>
-        BRZI VS. GORANCE <br><br>";
-    while ($row = mysqli_fetch_assoc($result)) {
-        echo $row['brzi'] . " : " . $row['gorance'] . ' --> ' . $row['playedOn'] . "<br>";
-    }   
+    if ($inputRadio == 'separate') {
+        $sql = "SELECT * FROM `brzi-gorance` WHERE `playedOn` BETWEEN '$inputDate1' AND '$inputDate2'";
+        $result = mysqli_query($conn, $sql);
+        $resultCheck = mysqli_num_rows($result);
+
+            echo "between $inputDate1 and $inputDate2 <br><br>
+            BRZI VS. GORANCE <br><br>";
+        while ($row = mysqli_fetch_assoc($result)) {
+            echo $row['brzi'] . " : " . $row['gorance'] . ' --> ' . $row['playedOn'] . "<br>";
+        }
+    } else {
+        $sql = "SELECT SUM(brzi), SUM(gorance) FROM `brzi-gorance` WHERE `playedOn` BETWEEN '$inputDate1' AND '$inputDate2'";
+        $result = mysqli_query($conn, $sql);
+        $resultCheck = mysqli_num_rows($result);
+
+            echo "between $inputDate1 and $inputDate2 <br><br>
+            BRZI VS. GORANCE <br><br>";
+        while ($row = mysqli_fetch_assoc($result)) {
+            echo $row['SUM(brzi)'] . " : " . $row['SUM(gorance)'] . "<br>";
+        }
+    }
+
 } else {
     echo "<br><br><br>
     Enter date to see their score!";
     }
+
+
+       /* mnogi drugi uslovi se treba srediti, ovo ispod je cisto da se isproba ideja
+    $input1 = $inputDate1 === '' ? $inputDate2 : $inputDate1;
+    $input2 = $inputDate2 === '' ? $inputDate1 : $inputDate2; */
 ?>

d114db9197e8975465c2799fdb18d2642b83760e
Milan Ivic <millan.ivic2343@gmail.com>
2022-06-06 14:54:31 +0200

    deleted unnessecary Update Score

--- TableFootball/showScore/echo.php
+++ TableFootball/showScore/echo.php
@@ -16,20 +16,21 @@ if ($_POST && $_POST['action'] == 'showScore') {
     $inputDate2 = date($_POST['date2']);
     $inputRadio = $_POST['score'];
     $dateRange = "between $inputDate1 and $inputDate2 <br><br> BRZI VS. GORANCE <br><br>";
+    echo $dateRange;
 
     if ($inputRadio == 'separate') {
+        $sql_column_name = "SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME='brzi-gorance'";
+        $result_column_name = mysqli_query($conn, $sql_column_name);
+        $resultCheck_column_name = mysqli_num_rows($result_column_name);
+        $row_column_name = mysqli_fetch_assoc($result_column_name);
+        echo '<table class="border">';
+        echo    '<tr>';
+        echo        "<th>" . $row_column_name['COLUMN_NAME'] . "</th>";
+        echo    "</tr>";
+        
         $sql = "SELECT * FROM `brzi-gorance` WHERE `playedOn` BETWEEN '$inputDate1' AND '$inputDate2'";
         $result = mysqli_query($conn, $sql);
         $resultCheck = mysqli_num_rows($result);
-        echo $dateRange;
-        echo 
-        '<table class="border"> 
-            <tr>
-                <th> Game ID </th>
-                <th> Brzi </th>
-                <th> Gorance </th>
-                <th> Date Played </th>
-            </tr>';
         while ($row = mysqli_fetch_assoc($result)) {
         echo    '<tr>';
         echo        "<td>" . $row['game-id'] . "</td>";

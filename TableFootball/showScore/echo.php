240ddbab7e609004304c62dbc6bd7f1e71b10532
Milan Ivic <millan.ivic2343@gmail.com>
2022-06-09 13:56:07 +0200

    showScore if statement input nicknames

--- TableFootball/showScore/echo.php
+++ TableFootball/showScore/echo.php
@@ -12,57 +12,61 @@ while ($row_Sum = mysqli_fetch_assoc($sum_Result)) {
 }
 
 if ($_POST && $_POST['action'] == 'showScore') {
+    $input_player_nick_or_id_1 = $_POST['player_nick_or_id_1'];
+    $input_player_nick_or_id_2 = $_POST['player_nick_or_id_2'];
     $inputDate1 = date($_POST['date1']);
     $inputDate2 = date($_POST['date2']);
     $inputRadio = $_POST['score'];
     $dateRange = "between $inputDate1 and $inputDate2 <br><br> BRZI VS. GORANCE <br><br>";
     echo $dateRange;
 
-
-    // $sql_column_names = "SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME='brzi_vs_gorance'";
-    if ($inputRadio == 'separate') {
-        $ordinal_Position = [1, 2];
-        $sql_column_names = "SELECT COLUMN_NAME 
-        FROM INFORMATION_SCHEMA.COLUMNS 
-        WHERE TABLE_NAME='brzi_vs_gorance'";
-        $result_column_names = mysqli_query($conn, $sql_column_names);
-            echo '<table class="border">';
-            echo    '<tr>';
-            while ($row_column_names = mysqli_fetch_assoc($result_column_names)) {
-                echo    "<th>" . $row_column_names['COLUMN_NAME'] . "</th>";
-            }
-            echo    "</tr>";
-        
-        $sql = "SELECT * FROM `brzi_vs_gorance` WHERE `playedOn` BETWEEN '$inputDate1' AND '$inputDate2'";
-        $result = mysqli_query($conn, $sql);
-        $resultCheck = mysqli_num_rows($result);
-        while ($row = mysqli_fetch_assoc($result)) {
-        echo    '<tr>';
-        echo        "<td>" . $row['game-id'] . "</td>";
-        echo        "<td>" . $row['playedOn'] . "</td>";
-        echo        "<td>" . $row['brzi'] . "</td>";
-        echo        "<td>" . $row['gorance'] . "</td>";
-        echo    "</tr>";
-        }
-        echo "</table>";
-    } else {
-        $sql = "SELECT SUM(brzi), SUM(gorance) FROM `brzi_vs_gorance` WHERE `playedOn` BETWEEN '$inputDate1' AND '$inputDate2'";
-        $result = mysqli_query($conn, $sql);
-        echo $dateRange;
-        while ($row = mysqli_fetch_assoc($result)) {
-            echo '<table class="border"> 
-            <tr>
-                <th> Brzi </th>
-                <th> Gorance </th>
-            </tr>';
-
-            echo '<tr>';
-            echo    '<td>' . $row['SUM(brzi)'] . '</td>';
-            echo    '<td>' . $row['SUM(gorance)'] . '</td>';
-            echo '</tr>';
-            echo '</table>';
-        }
+if ($input_player_nick_or_id_1 = 'brzi' && $input_player_nick_or_id_2 = 'gorance' ||
+    $input_player_nick_or_id_1 = 'gorance' && $input_player_nick_or_id_2 = 'brzi') {
+        $table_name = `brzi_vs_gorance`;
     }
+
+if ($inputRadio == 'separate') {
+    $sql_column_names = "SELECT COLUMN_NAME 
+    FROM INFORMATION_SCHEMA.COLUMNS 
+    WHERE TABLE_NAME=$table_name";
+    $result_column_names = mysqli_query($conn, $sql_column_names);
+        echo '<table class="border">';
+        echo    '<tr>';
+        while ($row_column_names = mysqli_fetch_assoc($result_column_names)) {
+            echo    "<th>" . $row_column_names['COLUMN_NAME'] . "</th>";
+        }
+        echo    "</tr>";
+        
+    $sql = "SELECT * FROM $table_name WHERE `playedOn` BETWEEN '$inputDate1' AND '$inputDate2'";
+    $result = mysqli_query($conn, $sql);
+    $resultCheck = mysqli_num_rows($result);
+    while ($row = mysqli_fetch_assoc($result)) {
+    echo    '<tr>';
+    echo        "<td>" . $row['game-id'] . "</td>";
+    echo        "<td>" . $row['playedOn'] . "</td>";
+    echo        "<td>" . $row['brzi'] . "</td>";
+    echo        "<td>" . $row['gorance'] . "</td>";
+    echo    "</tr>";
+    }
+    echo "</table>";
+} else {
+    $sql = "SELECT SUM(brzi), SUM(gorance) FROM $table_name WHERE `playedOn` BETWEEN '$inputDate1' AND '$inputDate2'";
+    $result = mysqli_query($conn, $sql);
+    echo $dateRange;
+    while ($row = mysqli_fetch_assoc($result)) {
+        echo '<table class="border"> 
+        <tr>
+            <th> Brzi </th>
+            <th> Gorance </th>
+        </tr>';
+
+        echo '<tr>';
+        echo    '<td>' . $row['SUM(brzi)'] . '</td>';
+        echo    '<td>' . $row['SUM(gorance)'] . '</td>';
+        echo '</tr>';
+        echo '</table>';
+    }
+}
 
     
 

b09acc21d287a15ca54e64d75277bd749c13ecfe
Milan Ivic <millan.ivic2343@gmail.com>
2022-09-02 11:04:58 +0200

    .

--- Friday13th/Friday13th.php
+++ Friday13th/Friday13th.php
@@ -3,35 +3,33 @@
 class Friday13thCalculator {
     protected $day_week;
     protected $day_num;
-    protected $start_year;
-    protected $end_year;
+    protected $year1;
+    protected $year2;
 
-    function __construct ($day_week, $day_num, $start_year, $end_year) {
+    function __construct ($day_week, $day_num, $year1, $year2) {
         $this->day_week = $day_week;
         $this->day_num = $day_num;
-        $this->start_year = $start_year;
-        $this->end_year = $end_year;
+        $this->year1 = $year1;
+        $this->year2 = $year2;
     }
 
     function calcMethod() {
         $counter = 0;
-        if ($this->start_year > 999 && $this->end_year > 999) {
-            for($year = $this->start_year; $year <= $this->end_year; $year++) {
+        if ($year1 <= $year2) {
+            for($year = $this->year1; $year <= $this->year2; $year++) {
                 for($month = 1; $month <= 12; $month++) {
                     $date = date_create("$year-$month-$this->day_num");
                     $x_th = date_format($date, "$this->day_num/M/Y");
                     $l_day = date('l', mktime(0,0,0,$month,$this->day_num,$year)); 
-                        if($l_day == $this->day_week) {
+                    if($l_day == $this->day_week) {
                         echo $l_day. " the ". $x_th. " ". "<br>";
                         $counter += 1;
-                        }
+                    }
                 }
                 echo $counter . "x $this->day_week" . "s in $year" . "<br>";
                 $counter = 0;
                 echo "<br>";
             }
-        } else {
-            echo "Please enter years from 1000 and above!";
         }
     }
 }
--- Friday13th/echo.php
+++ Friday13th/echo.php
@@ -5,10 +5,10 @@ include 'Friday13th.php';
 if ($_POST) {
     $day_week = $_POST['day_week'];
     $day_num = $_POST['day_num'];
-    $start_year = $_POST['start_year'];
-    $end_year = $_POST['end_year'];
+    $year1 = $_POST['year1'];
+    $year2 = $_POST['year2'];
     
-    $friday13thCalculator = new Friday13thCalculator($day_week, $day_num, $start_year, $end_year);
+    $friday13thCalculator = new Friday13thCalculator($day_week, $day_num, $year1, $year2);
     echo $friday13thCalculator->calcMethod();
 } else {
     echo "How many Fridays the 13th exist in 2020?";

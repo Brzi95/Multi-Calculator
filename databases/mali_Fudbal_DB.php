a75fec68481d432e225fc6ca5649a471f422bbba
Milan Ivic <millan.ivic2343@gmail.com>
2022-09-08 17:33:22 +0200

    .

---
+++ databases/mali_Fudbal_DB.php
@@ -0,0 +1,9 @@
+<?php
+$dbServername = "localhost";
+$dbUsername = "brzina";
+$dbPassword = "brzina95";
+$dbName = "mali fudbal";
+
+$conn2 = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
+
+
--- databases/table_Football_DB.php
+++ databases/table_Football_DB.php
@@ -5,3 +5,5 @@ $dbPassword = "brzina95";
 $dbName = "table football";
 
 $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
+
+

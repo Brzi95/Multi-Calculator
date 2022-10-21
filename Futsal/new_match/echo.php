26868295dc359e4df01d7864e90b6c2372e5df36
Milan Ivic <millan.ivic2343@gmail.com>
2022-10-21 19:53:32 +0200

    .

--- Futsal/new_match/echo.php
+++ Futsal/new_match/echo.php
@@ -3,27 +3,27 @@
 
 include 'forms-buttons_in_live_table.phtml';
 
-$sql_select_match = "SELECT first_name, last_name, goals, g.player_id, p.player_id AS pp_id, team_id FROM players p
-LEFT JOIN live_game g ON p.player_id = g.player_id
-WHERE team_id IS NULL -- select players who aren't in a team
-ORDER BY first_name";
-if ($result = mysqli_query($conn2, $sql_select_match)) {
-    if (mysqli_num_rows($result) > 0) {
-        while ($row = mysqli_fetch_array($result)) {
-            $id = $row['pp_id'];
-            $first_name = $row['first_name'];
-            $last_name = $row['last_name'];
+// $sql_select_match = "SELECT first_name, last_name, goals, g.player_id, p.player_id AS pp_id, team_id FROM players p
+// LEFT JOIN live_game g ON p.player_id = g.player_id
+// WHERE team_id IS NULL -- select players who aren't in a team
+// ORDER BY first_name";
+// if ($result = mysqli_query($conn2, $sql_select_match)) {
+//     if (mysqli_num_rows($result) > 0) {
+//         while ($row = mysqli_fetch_array($result)) {
+//             $id = $row['pp_id'];
+//             $first_name = $row['first_name'];
+//             $last_name = $row['last_name'];
 
-            $return_arr[] = array(
-                "id" => $id,
-                "first_name" => $first_name,
-                "last_name" => $last_name
-            );
-        }
-        json_encode($return_arr);
-    }
-}
-return;
+//             $return_arr[] = array(
+//                 "id" => $id,
+//                 "first_name" => $first_name,
+//                 "last_name" => $last_name
+//             );
+//         }
+//         json_encode($return_arr);
+//     }
+// }
+// return;
 
 // $player_id = $_GET['insert_player'] ?? null;
 // $team = $_GET['team_id'] ?? null;

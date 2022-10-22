13246e4384403e16b211c4f8c4f34b6bcadb9ed4
Milan Ivic <millan.ivic2343@gmail.com>
2022-10-22 21:54:42 +0200

    show player stats ajax php json

--- Futsal/statistics/echo2.php
+++ Futsal/statistics/echo2.php
@@ -1,41 +1,34 @@
 <?php
 
-$th = "<table class='border'>
-<tr>
-<td>IME</td>
-<td>PREZIME</td>
-<td>GOLOVI</td>
-<td>ASISTENCIJE</td>
-<td>PRISTUPIO</td>
-</tr>";
+include 'mali_Fudbal_DB.php';
 
-$q = $_GET['q'] ?? null;
-if ($q) {
-    include '../../databases/mali_Fudbal_DB.php';
-    $sql_get_player = "SELECT first_name, last_name, date_joined, SUM(goals) AS sum_goals, SUM(assists) AS sum_assists 
-    FROM futsal_games g
-    LEFT JOIN futsal_players p ON p.player_id = g.player_id
-    WHERE p.player_id = $q"
-    ;
-    if ($result = mysqli_query($conn2, $sql_get_player)) {
-        if (mysqli_num_rows($result) > 0) {
-            echo $th;
-            while ($row = mysqli_fetch_array($result)) {
-                date_timestamp_set(date_create(), $row['date_joined']);
-                $date = date_format(date_create(), "Y-m-d");
-
-                echo "<tr>";
-                echo "<td>". $row['first_name'] ."</td>";
-                echo "<td>". $row['last_name'] ."</td>";
-                echo "<td>". $row['sum_goals'] ."</td>";
-                echo "<td>". $row['sum_assists'] ."</td>";
-                echo "<td>". $date ."</td>";
-                echo "</tr>";
-            }
-            echo "</table>";
-            echo "<br>";
+$player_id_form = $_GET['player_id'];
+$sql_get_player = "SELECT first_name, last_name, nick_name, date_joined, p.player_id AS player_id, goals, assists 
+FROM futsal_games g
+LEFT JOIN futsal_players p ON p.player_id = g.player_id
+WHERE p.player_id = $player_id_form"
+;
+if ($result = mysqli_query($conn2, $sql_get_player)) {
+    if (mysqli_num_rows($result) > 0) {
+        while ($row = mysqli_fetch_array($result)) {
+            $player_id = $row['player_id'];
+            $first_name = $row['first_name'];
+            $last_name = $row['last_name'];
+            $nick_name = $row['nick_name'];
+            $goals = $row['goals'];
+            $assists = $row['assists'];
+            $date = date_format(date_create(), "Y-m-d");
+            
+            $arr_player[] = array(
+                "player_id" => $player_id,
+                "first_name" => $first_name,
+                "last_name" => $last_name,
+                "nick_name" => $nick_name,
+                "goals" => $goals,
+                "assists" => $assists,
+                "date" => $date
+            );
         }
-    } 
+        echo json_encode($arr_player);
+    }
 }
-
-

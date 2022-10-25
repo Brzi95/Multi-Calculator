520c97ad01f2c01c6efdd6968e1c32f612f3b4ec
Milan Ivic <millan.ivic2343@gmail.com>
2022-10-25 19:11:47 +0200

    .

--- Futsal/new_match/insert_player_into_live_table_2.php
+++ Futsal/new_match/insert_player_into_live_table_2.php
@@ -3,18 +3,18 @@
 include '../../databases/mali_Fudbal_DB.php';
 
 // INSERTING PLAYER INTO LIVE GAME TABLE
-$player_id = $_GET['player_id'] ?? null;
-$team_id = $_GET['team_id'] ?? null;
+$player_id_key = $_GET['player_id'] ?? null;
+$team_id_key = $_GET['team_id'] ?? null;
 $current_date = date_timestamp_get(date_create(date('Y-m-d')));
 
 $sql_insert_player = 
 "INSERT INTO `futsal_live_game` (`team_id`, `player_id`, `first_name`, `last_name`, `nick_name`, `goals`, `assists`, `date_of_game`, `game_id`) 
 VALUES (
-    $team_id, 
-    $player_id, 
-    (SELECT `first_name` FROM `futsal_players` WHERE `player_id` = $player_id),
-    (SELECT `last_name` FROM `futsal_players` WHERE `player_id` = $player_id),
-    (SELECT `nick_name` FROM `futsal_players` WHERE `player_id` = $player_id),
+    $team_id_key, 
+    $player_id_key, 
+    (SELECT `first_name` FROM `futsal_players` WHERE `player_id` = $player_id_key),
+    (SELECT `last_name` FROM `futsal_players` WHERE `player_id` = $player_id_key),
+    (SELECT `nick_name` FROM `futsal_players` WHERE `player_id` = $player_id_key),
     '0',
     '0',
     $current_date,
